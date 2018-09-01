3175aaa9158832ada96454fd96f6183fc9c70b23
DutchRascal <dutchrascal@fritz.box>
2018-09-01 14:33:45 +0200

    Added first script

--- Roll-A-Zombie/Assets/Tile.cs
+++ Roll-A-Zombie/Assets/Tile.cs
@@ -2,18 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour
+{
 
-   public GameManager gameManager;
-   public AudioClip hit;
-   AudioSource source;
+    /*   public GameManager gameManager;
+       public AudioClip hit;
+       AudioSource source;
 
-   void Start() {
-      source = GetComponent<AudioSource>();
-   }
+       void Start() {
+          source = GetComponent<AudioSource>();
+       }
 
-   void OnTriggerEnter(Collider other) {
-      gameManager.AddScore();
-      source.PlayOneShot(hit);
-   }
+       void OnTriggerEnter(Collider other) {
+          gameManager.AddScore();
+          source.PlayOneShot(hit);
+       } */
 }
